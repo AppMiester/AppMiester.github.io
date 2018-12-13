a1f14a8de454a240caf2b8f59984f2cfc94aeb35
AppMiester <36649282+AppMiester@users.noreply.github.com>
2018-12-12 20:13:08 -0500

    Update FishingMain.java

--- New folder/FishingMain.java
+++ New folder/FishingMain.java
@@ -2,18 +2,16 @@
 public class FishingMain extends FishingAction{
 
 	
-//static	Boat boat = new Boat();
+
 	
 
 
 	public static void main(String[] args) {
 		
-	//  AssaWampsett wamp = new AssaWampsett();
+	
 	  Johns john= new Johns();
 	  john.TryJohns();
-	 // wamp.TryAssaWampsett();
-	//	Boat boat = new Boat();
-		//boat.run();
+	
 		
 		
 		
